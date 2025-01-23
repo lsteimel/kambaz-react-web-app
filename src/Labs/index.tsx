4fd780df9704e4451e1c35989eabf32589cbadf3
Luke <50805440+lucic42@users.noreply.github.com>
2025-01-22 20:16:57 -0500

    stuff

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,6 +7,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <p>Luke Steimel, WebDev Section  02</p>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
