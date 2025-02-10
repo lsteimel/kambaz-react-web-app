69e09456fa3a169b5b0d25145d2159b22866ab18
Luke <50805440+lucic42@users.noreply.github.com>
2025-02-10 02:08:07 -0500

    fuix buldas

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -4,11 +4,11 @@ export default function AccountNavigation() {
   const { pathname } = useLocation();
   
   return (
-    <div id="wd-account-navigation" className="list-group fs-5 rounded-0">
+    <div id="wd-account-navigation" className="list-group fs-5 rounded-0 wd-sub-navigation">
       <Link
         to="/Kambaz/Account/Signin"
         id="wd-account-signin-link"
-        className={`list-group-item list-group-item-action border-0 ${pathname.includes("/Signin") ? "active" : "text-danger"}`}
+        className={`list-group-item list-group-item-action border-0 text-dark ${pathname.includes("/Signin") ? "active" : ""}`}
       >
         <div className="d-flex justify-content-between align-items-center">
           <span>Sign In</span>
@@ -18,7 +18,7 @@ export default function AccountNavigation() {
       <Link
         to="/Kambaz/Account/Signup" 
         id="wd-account-signup-link"
-        className={`list-group-item list-group-item-action border-0 ${pathname.includes("/Signup") ? "active" : "text-danger"}`}
+        className={`list-group-item list-group-item-action border-0 text-dark ${pathname.includes("/Signup") ? "active" : ""}`}
       >
         <div className="d-flex justify-content-between align-items-center">
           <span>Sign Up</span>
@@ -28,7 +28,7 @@ export default function AccountNavigation() {
       <Link
         to="/Kambaz/Account/Profile"
         id="wd-account-profile-link"
-        className={`list-group-item list-group-item-action border-0 ${pathname.includes("/Profile") ? "active" : "text-danger"}`}
+        className={`list-group-item list-group-item-action border-0 text-dark ${pathname.includes("/Profile") ? "active" : ""}`}
       >
         <div className="d-flex justify-content-between align-items-center">
           <span>Profile</span>
--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 import { FormControl } from "react-bootstrap";
 export default function Signup() {
