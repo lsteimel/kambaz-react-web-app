8bd2d15643e3d52e2ec32e9c70d09c3b6ac873cd
Luke <50805440+lucic42@users.noreply.github.com>
2025-04-25 15:39:25 -0400

    lab 5

---
+++ src/Labs/Lab5/WorkingWithObjects.tsx
@@ -0,0 +1,151 @@
+import { useState } from "react";
+import { FormControl } from "react-bootstrap";
+const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
+export default function WorkingWithObjects() {
+  const [assignment, setAssignment] = useState({
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+  });
+  const ASSIGNMENT_API_URL = `${REMOTE_SERVER}/lab5/assignment`;
+
+  const [module, setModule] = useState({
+    id: 1,
+    name: "Mod",
+    description: "a diy module example",
+    course: "web dev",
+  });
+  const MODULE_API_URL = `${REMOTE_SERVER}/lab5/module`;
+
+  return (
+    <div id="wd-working-with-objects">
+      <h3>Working With Objects</h3>
+      <h4>Modifying Properties</h4>
+      <a
+        id="wd-update-assignment-title"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/title/${assignment.title}`}
+      >
+        Update Title
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-assignment-title"
+        defaultValue={assignment.title}
+        onChange={(e) =>
+          setAssignment({ ...assignment, title: e.target.value })
+        }
+      />
+      <br />
+      <a
+        id="wd-update-assignment-title"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/score/${assignment.score}`}
+      >
+        Update Score
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-assignment-title"
+        type="number"
+        defaultValue={assignment.score}
+        onChange={(e) =>
+          setAssignment({ ...assignment, score: Number(e.target.value) })
+        }
+      />
+      <br />
+
+      <a
+        id="wd-update-assignment-title"
+        className="btn btn-primary float-end"
+        href={`${ASSIGNMENT_API_URL}/completed/${assignment.completed}`}
+      >
+        Update Completion
+      </a>
+
+      <div className="form-check mt-2">
+        <input
+          className="form-check-input"
+          type="checkbox"
+          id="assignment-published"
+          checked={assignment.completed}
+          onChange={(e) =>
+            setAssignment({ ...assignment, completed: e.target.checked })
+          }
+        />
+        <label className="form-check-label" htmlFor="assignment-published">
+          Completed?
+        </label>
+      </div>
+
+      <br />
+      <a
+        id="wd-update-module-name"
+        className="btn btn-primary float-end"
+        href={`${MODULE_API_URL}/name/${module.name}`}
+      >
+        Update Name
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-module-name"
+        defaultValue={module.name}
+        onChange={(e) => setModule({ ...module, name: e.target.value })}
+      />
+      <br />
+      <a
+        id="wd-update-module-name"
+        className="btn btn-primary float-end"
+        href={`${MODULE_API_URL}/description/${module.name}`}
+      >
+        Update Description
+      </a>
+      <FormControl
+        className="w-75"
+        id="wd-module-name"
+        defaultValue={module.description}
+        onChange={(e) => setModule({ ...module, description: e.target.value })}
+      />
+      <hr />
+      <h4>Retrieving Objects</h4>
+      <a
+        id="wd-retrieve-assignments"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/assignment`}
+      >
+        Get Assignment
+      </a>
+      <br />
+      <br />
+      <a
+        id="wd-retrieve-assignments"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/module`}
+      >
+        Get Module
+      </a>
+      <hr />
+      <h4>Retrieving Properties</h4>
+      <a
+        id="wd-retrieve-assignment-title"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/assignment/title`}
+      >
+        Get Title
+      </a>
+      <br />
+      <br />
+      <a
+        id="wd-retrieve-assignment-title"
+        className="btn btn-primary"
+        href={`${REMOTE_SERVER}/lab5/module/name`}
+      >
+        Get Name
+      </a>
+      <hr />
+    </div>
+  );
+}
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -4,6 +4,7 @@ import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
+import Lab5 from "./Lab5";
 
 import Kambaz from "../Kambaz";
 export default function Labs() {
@@ -22,6 +23,7 @@ export default function Labs() {
         <Route path="Lab2/*" element={<Lab2 />} />
         <Route path="Lab3" element={<Lab3 />} />
         <Route path="Lab4" element={<Lab4 />} />
+        <Route path="Lab5" element={<Lab5 />} />
         <Route path="Kambaz" element={<Kambaz />} />
       </Routes>
     </div>
